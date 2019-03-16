5dc650f626a29d6a06ff0130434496b84fe3db2d
rahulrssk <rahulrssk@gmail.com>
2019-03-16 11:23:49 +0530

    update

---
+++ app/src/main/java/com/ibt/niramaya/ui/activity/DocumentsActivity.java
@@ -0,0 +1,23 @@
+package com.ibt.niramaya.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.ibt.niramaya.R;
+import com.ibt.niramaya.utils.BaseActivity;
+
+public class DocumentsActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_documents);
+
+        initViews();
+
+    }
+
+    private void initViews() {
+
+    }
+}
