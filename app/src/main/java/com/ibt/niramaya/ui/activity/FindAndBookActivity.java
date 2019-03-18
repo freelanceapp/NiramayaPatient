9ef3978bb83049d0351894155c13ed3e25db6494
amardeep <amardeep.infobite@gmail.com>
2019-03-18 17:01:57 +0530

    new

--- app/src/main/java/com/ibt/niramaya/ui/activity/FindAndBookActivity.java
+++ app/src/main/java/com/ibt/niramaya/ui/activity/FindAndBookActivity.java
@@ -1,23 +1,24 @@
 package com.ibt.niramaya.ui.activity;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.TextView;
 
 import com.ibt.niramaya.R;
+import com.ibt.niramaya.utils.BaseActivity;
 
-public class PeditricianActivity extends AppCompatActivity {
+public class FindAndBookActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_peditrician);
+        setContentView(R.layout.activity_find_and_book);
 
         init();
     }
 
     private void init() {
-        findViewById(R.id.imgSearch).setVisibility(View.VISIBLE);
+        ((TextView) findViewById(R.id.txtTitle)).setText(getString(R.string.find_and_book));
         findViewById(R.id.imgFilter).setVisibility(View.VISIBLE);
     }
 }
