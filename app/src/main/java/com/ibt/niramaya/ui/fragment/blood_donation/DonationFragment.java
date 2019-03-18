3a2c548a09d64f25809d0a19af566800c9c91fe0
amardeep12 <amardeep.infobite@gmail.com>
2019-03-18 18:18:14 +0530

    update

--- app/src/main/java/com/ibt/niramaya/ui/fragment/blood_donation/DonationFragment.java
+++ app/src/main/java/com/ibt/niramaya/ui/fragment/blood_donation/DonationFragment.java
@@ -1,17 +1,25 @@
 package com.ibt.niramaya.ui.fragment.blood_donation;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.ibt.niramaya.R;
 import com.ibt.niramaya.utils.BaseFragment;
 
+import java.util.Calendar;
+
 public class DonationFragment extends BaseFragment {
     private View rootview;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -23,56 +31,143 @@ public class DonationFragment extends BaseFragment {
     private void init() {
     }
 
-    private void openDonorDialog(){
+    private void openDonorDialogOne() {
         AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(mContext);
         dialogBuilder.setCancelable(false);
 
+        LayoutInflater inflater = this.getLayoutInflater();
+        final View dialogView = inflater.inflate(R.layout.dialog_become_doner_one, null);
+        dialogBuilder.setView(dialogView);
+        final AlertDialog alertDialog = dialogBuilder.create();
+
+        ((EditText) dialogView.findViewById(R.id.etDOB)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openDatePicker(((EditText) dialogView.findViewById(R.id.etDOB)));
+            }
+        });
+
+        ((EditText) dialogView.findViewById(R.id.etLastDate)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openDatePicker(((EditText) dialogView.findViewById(R.id.etLastDate)));
+            }
+        });
+
+        ((Button) dialogView.findViewById(R.id.btnNext)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                initDialogOneData(dialogView, alertDialog);
+            }
+        });
+
+        alertDialog.show();
     }
 
-    /*private fun showDialog(studentIds: String) {
-        val dialogBuilder = AlertDialog.Builder(mContext)
-        //dialogBuilder.tit
-        dialogBuilder.setCancelable(false)
+    private void openDatePicker(final EditText etDate) {
+        int dobYear = Calendar.getInstance().get(Calendar.YEAR);
+        int dobMonth = Calendar.getInstance().get(Calendar.MONTH);
+        int dobDay = Calendar.getInstance().get(Calendar.DAY_OF_MONTH);
+        DatePickerDialog dialog = new DatePickerDialog(mContext, R.style.DialogTheme, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
 
-        val inflater = this.layoutInflater
-        val dialogView = inflater.inflate(R.layout.dialog_add_team, null)
-        dialogBuilder.setView(dialogView)
-        val alertDialog = dialogBuilder.create()
-        val btnCreate = dialogView.findViewById<Button>(R.id.btnDialogCreate)
-        val btnCancel = dialogView.findViewById<Button>(R.id.btnDialogCancel)
-        val etTeamName = dialogView.findViewById<EditText>(R.id.etAddTeam)
-        val spnBatch = dialogView.findViewById<Spinner>(R.id.spnBatch)
-        //msgBox.filters = InputFilter[](InputFilter())
+                String sDay;
+                String sMonth;
 
-
-        val batchAdapter = ArrayAdapter(mContext, R.layout.spinner_item_team, batchArray)
-        spnBatch.adapter = batchAdapter
-        spnBatch.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-            override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
-                if (position > 0) {
-                    sBatch = batchArray[position]
-                }else{
-                    sBatch = ""
+                if (day <= 9) {
+                    sDay = "0" + day;
+                } else {
+                    sDay = String.valueOf(day);
                 }
-            }
+                if ((month + 1) <= 9) {
+                    sMonth = "0" + (month + 1);
+                } else {
+                    sMonth = String.valueOf((month + 1));
+                }
 
-            override fun onNothingSelected(parent: AdapterView<*>) {
+                etDate.setText(sDay + "/" + sMonth + "/" + year);
 
             }
+        }, dobYear, dobMonth, dobDay);
+        dialog.getDatePicker().setMaxDate(System.currentTimeMillis() - 1000);
+        dialog.setTitle("");
+        dialog.show();
+    }
+
+    private void initDialogOneData(View dialogView, AlertDialog alertDialog) {
+        String dName, dEmail, dMobile, dAlternateMobile, dDOB, dGender, dBloodGroup,
+                dWeight, dAddress, dLastDate;
+        dName = ((EditText) dialogView.findViewById(R.id.etName)).getText().toString().trim();
+        dEmail = ((EditText) dialogView.findViewById(R.id.etEmail)).getText().toString().trim();
+        dMobile = ((EditText) dialogView.findViewById(R.id.etMobile)).getText().toString().trim();
+        dAlternateMobile = ((EditText) dialogView.findViewById(R.id.etAlternateMobile)).getText().toString().trim();
+        dDOB = ((EditText) dialogView.findViewById(R.id.etDOB)).getText().toString().trim();
+        dGender = ((EditText) dialogView.findViewById(R.id.etGender)).getText().toString().trim();
+        dBloodGroup = ((EditText) dialogView.findViewById(R.id.etBloodGroup)).getText().toString().trim();
+        dWeight = ((EditText) dialogView.findViewById(R.id.etWeight)).getText().toString().trim();
+        dAddress = ((EditText) dialogView.findViewById(R.id.etAddress)).getText().toString().trim();
+        dLastDate = ((EditText) dialogView.findViewById(R.id.etLastDate)).getText().toString().trim();
+
+        if (dName.isEmpty()){
+            showToast("Name should not be empty.");
+        }else if(dEmail.isEmpty()){
+            showToast("Email should not be empty.");
+        }else if(dMobile.isEmpty()){
+            showToast("Mobile should not be empty.");
+        }else if(dDOB.isEmpty()){
+            showToast("Date of Birth should not be empty.");
+        }else if(dGender.isEmpty()){
+            showToast("Date of Birth should not be empty.");
+        }else if(dBloodGroup.isEmpty()){
+            showToast("Blood Group should not be empty.");
+        }else if(dWeight.isEmpty()){
+            showToast("Weight should not be empty.");
+        }else if(dAddress.isEmpty()){
+            showToast("Address should not be empty.");
+        }else if(dLastDate.isEmpty()){
+            showToast("Last Date should not be empty.");
+        }else{
+            openDonorDialogTwo();
+            alertDialog.dismiss();
         }
+    }
 
-        btnCancel.setOnClickListener { alertDialog.dismiss() }
-        btnCreate.setOnClickListener {
-            val teamName =  etTeamName.text.toString().trim()
-            if (teamName.isEmpty()){
-                showToast(mContext, "Write a team name!")
-            }else if (sBatch.isEmpty()){
-                showToast(mContext, "No Batch selected!")
-            }else{
-                createTeam(teamName, studentIds)
+    private void openDonorDialogTwo() {
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(mContext);
+        dialogBuilder.setCancelable(false);
+
+        LayoutInflater inflater = this.getLayoutInflater();
+        final View dialogView = inflater.inflate(R.layout.dialog_become_doner_one, null);
+        dialogBuilder.setView(dialogView);
+        final AlertDialog alertDialog = dialogBuilder.create();
+
+        /*((EditText) dialogView.findViewById(R.id.etDOB)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openDatePicker(((EditText) dialogView.findViewById(R.id.etDOB)));
             }
-            alertDialog.dismiss()
-        }
-        alertDialog.show()
-    }*/
+        });
+
+        ((EditText) dialogView.findViewById(R.id.etLastDate)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openDatePicker(((EditText) dialogView.findViewById(R.id.etLastDate)));
+            }
+        });
+
+        ((Button) dialogView.findViewById(R.id.btnNext)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                initDialogOneData(dialogView, alertDialog);
+            }
+        });*/
+
+        alertDialog.show();
+    }
+
+    private void showToast(String msg){
+        Toast.makeText(mContext, msg, Toast.LENGTH_SHORT).show();
+    }
+
 }
